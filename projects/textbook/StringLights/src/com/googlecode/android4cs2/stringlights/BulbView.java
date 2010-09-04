bc78fd9ab0788c0d6ec3bdf4f08449c06fddead1
jdjennin <jdjennin@7160e6b0-a975-b924-3813-ee829f2ed0ed>
2010-09-04 15:56:20 +0000

    Made a 8-bulb light string which the user can play with and randomize. Thinking about adding a glint on the colored bulbs for consistency with other light bulb graphics I've seen.

--- projects/textbook/StringLights/src/com/googlecode/android4cs2/stringlights/BulbView.java
+++ projects/textbook/StringLights/src/com/googlecode/android4cs2/stringlights/BulbView.java
@@ -1,7 +1,9 @@
 package com.googlecode.android4cs2.stringlights;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.widget.ImageView;
@@ -10,26 +12,38 @@ import android.widget.ImageView;
 public class BulbView extends ImageView implements GestureDetector.OnGestureListener  {
 	
 	private ColoredLight bulb;
+	private GestureDetector detector;
 
 	public BulbView(Context context) {
 		super(context);
 		bulb = new ColoredLight();
+		detector = new GestureDetector(this);
+		this.setImageResource(R.drawable.bulboff);
 	}
 
 	public BulbView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		bulb = new ColoredLight();
+		detector = new GestureDetector(this);
+		this.setImageResource(R.drawable.bulboff);
 	}
 
 	public BulbView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 		bulb = new ColoredLight();
+		detector = new GestureDetector(this);
+		this.setImageResource(R.drawable.bulboff);
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		return detector.onTouchEvent(event);
 	}
 
 	@Override
 	public boolean onDown(MotionEvent event) {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
@@ -60,30 +74,65 @@ public class BulbView extends ImageView implements GestureDetector.OnGestureList
 
 	@Override
 	public boolean onSingleTapUp(MotionEvent event) {
+		Log.d("BulbView:", "onSingleTapUp");
 		if (bulb.isOn()) {
 			bulb.setOn(false);
 			this.setImageResource(R.drawable.bulboff);
+			Log.d("BulbView:", "Bulb is on.");
 		} else {
 			bulb.setOn(true);
 			switch (bulb.getColor()) {
 			case 'R':
-				// this.setImageResource(R.drawable.bulbred);
-				this.invalidate();
+				this.setImageResource(R.drawable.bulbred);
+				Log.d("BulbView:", "Bulb is red.");
 				break;
 			case 'G':
-				// this.setImageResource(R.drawable.bulbgreen);
-				this.invalidate();
+				this.setImageResource(R.drawable.bulbgreen);
+				Log.d("BulbView:", "Bulb is green.");
 				break;
 			case 'B':
-				// this.setImageResource(R.drawable.bulbblue);
-				this.invalidate();
+				this.setImageResource(R.drawable.bulbblue);
+				Log.d("BulbView:", "Bulb is blue.");
 				break;
 			default:
 				this.setImageResource(R.drawable.bulboff);
-				this.invalidate();
+				Log.d("BulbView:", "Bulb is off.");
 				break;
 			}
 		}
+		
+		this.invalidate();
 		return true;
 	}
+	
+	public void randomize() {
+		bulb = new ColoredLight();
+		
+		if (bulb.isOn()) {
+			switch (bulb.getColor()) {
+			case 'R':
+				this.setImageResource(R.drawable.bulbred);
+				Log.d("BulbView:", "Bulb is red.");
+				break;
+			case 'G':
+				this.setImageResource(R.drawable.bulbgreen);
+				Log.d("BulbView:", "Bulb is green.");
+				break;
+			case 'B':
+				this.setImageResource(R.drawable.bulbblue);
+				Log.d("BulbView:", "Bulb is blue.");
+				break;
+			default:
+				this.setImageResource(R.drawable.bulboff);
+				Log.d("BulbView:", "Bulb is off.");
+				break;
+			}
+		} else {
+			this.setImageResource(R.drawable.bulboff);
+		}
+		
+		this.invalidate();
+		
+	}
 }
+
--- projects/textbook/StringLights/src/com/googlecode/android4cs2/stringlights/LightStringDemo.java
+++ projects/textbook/StringLights/src/com/googlecode/android4cs2/stringlights/LightStringDemo.java
@@ -1,8 +1,12 @@
 package com.googlecode.android4cs2.stringlights;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ToggleButton;
 
 /** 
@@ -12,9 +16,11 @@ import android.widget.ToggleButton;
  *
  */
 public class LightStringDemo extends Activity {
-	
+
+	private LinearLayout linLay;
 	private Button random;
 	private ToggleButton blink;
+	private ArrayList<BulbView> lights;
 
 	/**
 	 * This is the entry point for any Android Activity.
@@ -28,9 +34,31 @@ public class LightStringDemo extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        lights = new ArrayList<BulbView>();
+        
+        linLay = (LinearLayout) findViewById(R.id.linLay);
+        for (int i = 0; i < 8; i++) {
+        	lights.add(new BulbView(this));
+        	linLay.addView(lights.get(i));
+        }
+
         random = (Button) findViewById(R.id.random);
+        random.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				randomize();
+			}
+        	
+        });
         
         blink = (ToggleButton) findViewById(R.id.blink);
         
+    }
+    
+    public void randomize() {
+		for (BulbView v: lights) {
+			v.randomize();
+		}
     }
 }
