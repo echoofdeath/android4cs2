08057b2f7865e58ae2232508f8e06713cbb5b555
jdjennin <jdjennin@7160e6b0-a975-b924-3813-ee829f2ed0ed>
2010-09-05 22:35:30 +0000

    Added numerous comments and generated javadocs.

--- trunk/projects/textbook/StringLights/src/com/googlecode/android4cs2/stringlights/BulbView.java
+++ trunk/projects/textbook/StringLights/src/com/googlecode/android4cs2/stringlights/BulbView.java
@@ -6,12 +6,31 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.widget.ImageView;
 
-
+/**
+ * BulbView is a subclass of ImageView which is an implementation of the OnGestureListener interface.
+ * It contains information about the light bulb and handles all changes to the bulb's state through
+ * the user interface.
+ * @author jdjennin
+ * @see ImageView
+ * @see GestureDetector
+ */
 public class BulbView extends ImageView implements GestureDetector.OnGestureListener  {
 	
+	/** 
+	 * The light bulb
+	 */
 	private ColoredLight bulb;
+	
+	/**
+	 * Allows us to detect gestures
+	 */
 	private GestureDetector detector;
 
+	/**
+	 * Default constructor for a BulbView. Initializes a ColoredLight and the GestureDetector.
+	 * @param context
+	 * @see Context
+	 */
 	public BulbView(Context context) {
 		super(context);
 		bulb = new ColoredLight();
@@ -19,6 +38,13 @@ public class BulbView extends ImageView implements GestureDetector.OnGestureList
 		this.setImageResource(R.drawable.bulboff);
 	}
 
+	/**
+	 * This constructor is never explicitly called.
+	 * @param context
+	 * @param attrs
+	 * @see Context
+	 * @see AttributeSet
+	 */
 	public BulbView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		bulb = new ColoredLight();
@@ -26,6 +52,14 @@ public class BulbView extends ImageView implements GestureDetector.OnGestureList
 		this.setImageResource(R.drawable.bulboff);
 	}
 
+	/**
+	 * This constructor is never explicitly called.
+	 * @param context
+	 * @param attrs
+	 * @param defStyle
+	 * @see Context
+	 * @see AttributeSet
+	 */
 	public BulbView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 		bulb = new ColoredLight();
@@ -33,17 +67,33 @@ public class BulbView extends ImageView implements GestureDetector.OnGestureList
 		this.setImageResource(R.drawable.bulboff);
 	}
 	
+	/**
+	 * Defines what to do in the case of a Touch Event. In this View, we pass the event to our
+	 * GestureDetector.
+	 * @param event The MotionEvent which triggered this callback.
+	 * @see MotionEvent
+	 * @return boolean
+	 */
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		return detector.onTouchEvent(event);
 	}
 
+	/**
+	 * Defines what to do when one's finger touches the screen. It must return true if one hopes
+	 * to accomplish anything when touch events happen.
+	 * @param event The MotionEvent which triggered this callback--here, a finger touching the screen.
+	 * @see MotionEvent
+	 * @return boolean
+	 */
 	@Override
 	public boolean onDown(MotionEvent event) {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
+	/**
+	 * Defines what to do when a finger is flung across the screen. We don't care about that here.
+	 */
 	@Override
 	public boolean onFling(MotionEvent event, MotionEvent arg1, float arg2,
 			float arg3) {
@@ -51,12 +101,18 @@ public class BulbView extends ImageView implements GestureDetector.OnGestureList
 		return false;
 	}
 
+	/**
+	 * Defines what to do when a finger is held down for a prolonged period. Don't care.
+	 */
 	@Override
 	public void onLongPress(MotionEvent event) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * Defines what to do when a finger scrolls on the screen. Don't care.
+	 */
 	@Override
 	public boolean onScroll(MotionEvent event, MotionEvent arg1, float arg2,
 			float arg3) {
@@ -64,64 +120,108 @@ public class BulbView extends ImageView implements GestureDetector.OnGestureList
 		return false;
 	}
 
+	/**
+	 * Defines what to do when the user puts a finger on the screen.
+	 */
 	@Override
 	public void onShowPress(MotionEvent event) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * Defines what to do when a single touch was performed by the user on the screen.
+	 * In this case, we turn the bulb off and on based on its current status.
+	 * @param event The tap which triggered this callback.
+	 * @see MotionEvent
+	 * @return boolean
+	 */
 	@Override
 	public boolean onSingleTapUp(MotionEvent event) {
+		// If the bulb is on...
 		if (bulb.isOn()) {
+			// ...turn it off
 			turnOff();
 		} else {
+			// ...otherwise, it was off and must be turned on
 			turnOn();
 		}
 		return true;
 	}
 	
+	/**
+	 * Creates a new ColoredLight with the same state as the original but with a random color.
+	 * @return void
+	 */
 	public void randomize() {
+		// Save the previous state of the bulb
 		boolean state = bulb.isOn();
+		
+		// Create a new random ColoredLight
 		bulb = new ColoredLight();
 		
+		// If the old bulb was on...
 		if (state) {
+			// ...turn the new one on
 			turnOn();
 		} else {
+			// ...otherwise, keep it off
 			turnOff();
 		}
 	}
 	
+	/**
+	 * Turns on the bulb associated with the current view. Sets its image resource accordingly,
+	 * then invalidates the View so that it is redrawn.
+	 * @return void
+	 */
 	public void turnOn() {
+		// Set the state of the bulb to on
 		bulb.setOn(true);
+		// If the bulb's color is...
 		switch (bulb.getColor()) {
+		// ...red...
 		case 'R':
+			// ...change the Image Resource of the bulb to the red image
 			this.setImageResource(R.drawable.bulbred);
 			break;
+		// ...or if green...
 		case 'G':
+			// ...change the Image Resource to the green image
 			this.setImageResource(R.drawable.bulbgreen);
 			break;
+		// ...or if blue...
 		case 'B':
+			// ...change the Image Resource to the blue image
 			this.setImageResource(R.drawable.bulbblue);
 			break;
 		default:
+			// And if something crazy happened and the bulb has no color, just keep it off
 			this.setImageResource(R.drawable.bulboff);
 			break;
 		}
+		// Invalidate the View so that it is redrawn with the updated Image Resource 
 		this.invalidate();
 	}
 	
+	/**
+	 * Turns off the bulb associated with the current view. Sets its image resource accordingly,
+	 * then invalidates the View so that it is redrawn.
+	 * @return void
+	 */
 	public void turnOff() {
+		// Set the bulb's state to off, change the image, and redraw the View
 		bulb.setOn(false);
 		this.setImageResource(R.drawable.bulboff);
 		this.invalidate();
 	}
 	
+	/**
+	 * Returns the current status of the bulb associated with this View.
+	 * @return boolean
+	 */
 	public boolean isOn() {
 		return bulb.isOn();
-	}
-	
-	public Character getColor() {
-		return bulb.getColor();
 	}
 }
 
--- trunk/projects/textbook/StringLights/src/com/googlecode/android4cs2/stringlights/LightStringDemo.java
+++ trunk/projects/textbook/StringLights/src/com/googlecode/android4cs2/stringlights/LightStringDemo.java
@@ -20,8 +20,14 @@ import android.widget.ToggleButton;
  */
 public class LightStringDemo extends Activity {
 
+	/**
+	 * This acts as an interface for making changes to the UI from outside of the UI thread.
+	 */
 	final Handler hand = new Handler();
 	
+	/**
+	 * These are the changes to be made using the Handler object.
+	 */
 	final Runnable updateLights = new Runnable() {
 		public void run() {
 			for (BulbView v: lights) {
@@ -34,12 +40,39 @@ public class LightStringDemo extends Activity {
 		}
 	};
 	
+	/**
+	 * This layout holds all of the BulbViews
+	 */
 	private LinearLayout linLay;
+	
+	/**
+	 * Randomizes the colors of the BulbViews
+	 */
 	private Button random;
+	
+	/**
+	 * Turns on all lights
+	 */
 	private Button allOn;
+	
+	/**
+	 * Turns off all lights
+	 */
 	private Button allOff;
+	
+	/**
+	 * Toggles blinking
+	 */
 	private ToggleButton blink;
+	
+	/**
+	 * Stores pointers to the BulbViews
+	 */
 	private ArrayList<BulbView> lights;
+	
+	/**
+	 * A timer to be used when blinking is activated
+	 */
 	private Timer timer;
 
 	/**
@@ -54,16 +87,20 @@ public class LightStringDemo extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        // Initialize our Timer and ArrayList
         timer = new Timer();
         lights = new ArrayList<BulbView>();
         
+        // Get the layout so that we can add the BulbViews to it
         linLay = (LinearLayout) findViewById(R.id.linLay);
         
+        // Set up 8 BulbViews, save them to our ArrayList, and add them to the LinearLayout
         for (int i = 0; i < 8; i++) {
         	lights.add(new BulbView(this));
         	linLay.addView(lights.get(i));
         }
 
+        // Get the randomize Button so that we can define what it's supposed to do
         random = (Button) findViewById(R.id.random);
         random.setOnClickListener(new View.OnClickListener() {
 			
@@ -74,6 +111,7 @@ public class LightStringDemo extends Activity {
         	
         });
         
+        // Get the allOn Button so that we can define what it does
         allOn = (Button) findViewById(R.id.allOn);
         allOn.setOnClickListener(new View.OnClickListener() {
 			
@@ -90,6 +128,7 @@ public class LightStringDemo extends Activity {
 			}
 		});
         
+        // Get the allOff Button so that we can define what it does
         allOff = (Button) findViewById(R.id.allOff);
         allOff.setOnClickListener(new View.OnClickListener() {
 
@@ -107,14 +146,18 @@ public class LightStringDemo extends Activity {
         	
         });
         
+        // Get the blink ToggleButton so that we can set up blinking
         blink = (ToggleButton) findViewById(R.id.blink);
         blink.setOnClickListener(new View.OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
+				// If the ToggleButton was checked...
 				if (((ToggleButton)v).isChecked()) {
+					// ...start blinking the lights...
 					startBlinking();
 				} else {
+					// ...otherwise, the lights are already blinking and need to be stopped
 					stopBlinking();
 				}
 			}
@@ -123,22 +166,39 @@ public class LightStringDemo extends Activity {
         
     }
     
+    /** Randomizes all the BulbViews
+     * @return void
+     */
     public void randomize() {
 		for (BulbView b: lights) {
 			b.randomize();
 		}
     }
     
+    /** Helper method for starting blinking
+     * @return void
+     * @see Timer
+     * @see TimerTask
+     * @see Handler
+     */
     public void startBlinking() {
+    	// Using our Timer, we set up a TimerTask to be done without delay every 1000 milliseconds
     	timer.scheduleAtFixedRate(new TimerTask() {
 			@Override
 			public void run() {
+				/* Here is where our Handler object comes in handy. Pun absolutely intended.
+				 * We post the code we defined as the Runnable object updateLights to the Handler
+				 * where it is executed. */
 				hand.post(updateLights);
 			}
     		
     	}, 0, 1000);
     }
     
+    /** Helper method for stopping blinking
+     * @return void
+     * @see Timer
+     */
     public void stopBlinking() {
     	timer.cancel();
     	timer = new Timer();
