8ef6b058d609d66cc280c7e82dab8b8c7bfaec47
jdjennin <jdjennin@7160e6b0-a975-b924-3813-ee829f2ed0ed>
2010-09-07 20:58:59 +0000

    New project!

---
+++ projects/textbook/Domineering/src/com/android4cs2/googlecode/domineering/DomineeringActivity.java
@@ -0,0 +1,13 @@
+package com.android4cs2.googlecode.domineering;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DomineeringActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
