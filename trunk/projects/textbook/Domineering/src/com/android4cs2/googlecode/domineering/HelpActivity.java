d5d6953a19aa1052cfb2ec6ddd186ba3621f8885
jdjennin <jdjennin@7160e6b0-a975-b924-3813-ee829f2ed0ed>
2010-09-14 03:40:15 +0000

    Added options menu, fixed user switching, user cannot cause arrayindexoutofboundsexception, added a HelpActivity, and changed the constructor of ImageAdapter a to make code more general. The basis for orientation change is there if we can find a way to make it pretty.

---
+++ trunk/projects/textbook/Domineering/src/com/android4cs2/googlecode/domineering/HelpActivity.java
@@ -0,0 +1,14 @@
+package com.android4cs2.googlecode.domineering;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelpActivity extends Activity {
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.help_main);
+		
+	}
+	
+}
