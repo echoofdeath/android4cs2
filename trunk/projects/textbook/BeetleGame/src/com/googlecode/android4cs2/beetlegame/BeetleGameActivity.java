3a5c0f576f5de3f8dd82d98390563ae99b55c56d
jdjennin <jdjennin@7160e6b0-a975-b924-3813-ee829f2ed0ed>
2010-09-15 05:35:03 +0000

    Just created a new project. Nothing to see yet.

---
+++ trunk/projects/textbook/BeetleGame/src/com/googlecode/android4cs2/beetlegame/BeetleGameActivity.java
@@ -0,0 +1,13 @@
+package com.googlecode.android4cs2.beetlegame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BeetleGameActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
