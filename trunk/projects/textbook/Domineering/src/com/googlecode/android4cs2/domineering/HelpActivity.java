76ad6c40d86b0d5e436fd970af4c2c5cae4637d0
jdjennin <jdjennin@7160e6b0-a975-b924-3813-ee829f2ed0ed>
2010-09-15 05:19:51 +0000

    Generated Javadoc.

--- trunk/projects/textbook/Domineering/src/com/googlecode/android4cs2/domineering/HelpActivity.java
+++ trunk/projects/textbook/Domineering/src/com/googlecode/android4cs2/domineering/HelpActivity.java
@@ -3,8 +3,10 @@ package com.googlecode.android4cs2.domineering;
 import android.app.Activity;
 import android.os.Bundle;
 
+/** This Activity tells the user the point of the game. */
 public class HelpActivity extends Activity {
 	
+	/** This method is called when the Activity is first created. */
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.help_main);
