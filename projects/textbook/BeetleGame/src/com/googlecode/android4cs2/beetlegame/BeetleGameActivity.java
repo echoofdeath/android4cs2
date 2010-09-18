bd6ff35138b5b78fb92e6e85d6e82e4ea6d8f5e9
jdjennin <jdjennin@7160e6b0-a975-b924-3813-ee829f2ed0ed>
2010-09-18 03:38:57 +0000

    Basic layout, die rolling.

--- projects/textbook/BeetleGame/src/com/googlecode/android4cs2/beetlegame/BeetleGameActivity.java
+++ projects/textbook/BeetleGame/src/com/googlecode/android4cs2/beetlegame/BeetleGameActivity.java
@@ -2,12 +2,58 @@ package com.googlecode.android4cs2.beetlegame;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class BeetleGameActivity extends Activity {
     /** Called when the activity is first created. */
+	
+	private ImageView die;
+	private TextView turn;
+	private OnClickListener dieListener;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        die = (ImageView) findViewById(R.id.die);
+        turn = (TextView) findViewById(R.id.turn);
+        
+        dieListener = new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				switch ((int)(Math.random()*6)+1) {
+				case 1:
+					die.setImageResource(R.drawable.die1);
+					break;
+				case 2:
+					die.setImageResource(R.drawable.die2);
+					break;
+				case 3:
+					die.setImageResource(R.drawable.die3);
+					break;
+				case 4:
+					die.setImageResource(R.drawable.die4);
+					break;
+				case 5:
+					die.setImageResource(R.drawable.die5);
+					break;
+				case 6:
+					die.setImageResource(R.drawable.die6);
+					break;
+				default:
+					break;
+				}
+			}
+        	
+        };
+        
+        die.setOnClickListener(dieListener);
+        
     }
 }
