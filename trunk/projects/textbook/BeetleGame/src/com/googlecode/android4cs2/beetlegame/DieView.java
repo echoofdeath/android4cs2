0b5251083b284601d2604fc30142f03b9877f492
jdjennin <jdjennin@7160e6b0-a975-b924-3813-ee829f2ed0ed>
2010-09-20 04:21:08 +0000

    Null pointer exception, need pointer to BeetleSurface within DieView

--- trunk/projects/textbook/BeetleGame/src/com/googlecode/android4cs2/beetlegame/DieView.java
+++ trunk/projects/textbook/BeetleGame/src/com/googlecode/android4cs2/beetlegame/DieView.java
@@ -2,62 +2,73 @@ package com.googlecode.android4cs2.beetlegame;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.ImageView;
 import android.view.MotionEvent;
 
 public class DieView extends ImageView {
 
 	private Die d;
-	private BeetleGame game;
+	private BeetleSurface bs;
 	
 	public DieView(Context context) {
 		super(context);
 		setImageResource(R.drawable.die4);
-		game = new BeetleGame();
-		d = game.getDie();
+		d = new Die();
+		Log.d("Con1.", "");
 	}
 
 	public DieView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		setImageResource(R.drawable.die4);
-		game = new BeetleGame();
-		d = game.getDie();
+		d = new Die();
+		Log.d("Con2.", "");
 	}
 
 	public DieView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 		setImageResource(R.drawable.die4);
-		game = new BeetleGame();
-		d = game.getDie();
+		d = new Die();
+		Log.d("Con3.", "");
 	}
 
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		if ((event.getAction() & MotionEvent.ACTION_MASK) == MotionEvent.ACTION_DOWN) {
 			d.roll();
+			// Problem here: null pointer exception. Need pointer to BeetleSurface
+			Beetle current = bs.getBug();
 			switch (d.getTopFace()) {
 			case 1:
 				setImageResource(R.drawable.die1);
+				current.addBody();
 				break;
 			case 2:
 				setImageResource(R.drawable.die2);
+				current.addHead();
 				break;
 			case 3:
 				setImageResource(R.drawable.die3);
+				current.addLeg();
 				break;
 			case 4:
 				setImageResource(R.drawable.die4);
+				current.addEye();
 				break;
 			case 5:
 				setImageResource(R.drawable.die5);
+				current.addFeeler();
 				break;
 			case 6:
 				setImageResource(R.drawable.die6);
+				// current.addTail();
 				break;
 			default:
 				break;
 			}
 			invalidate();
+			bs.invalidate();
+			
 			return true;
 		}
 		return false;
@@ -65,9 +76,5 @@ public class DieView extends ImageView {
 	
 	public Die getDie() {
 		return d;
-	}
-	
-	public BeetleGame getGame() {
-		return game;
 	}
 }
