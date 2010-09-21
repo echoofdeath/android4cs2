d178f8e66159ae0d612b72603e4cfed01a06c861
jdjennin <jdjennin@7160e6b0-a975-b924-3813-ee829f2ed0ed>
2010-09-21 03:46:31 +0000

    Changed the parent of BeetleSurface to ImageView and renamed it BeetleView. Resized the images to 200x200. Rolling the die and drawing bug parts works. Player switching to come next.

--- projects/textbook/BeetleGame/src/com/googlecode/android4cs2/beetlegame/BeetleGameActivity.java
+++ projects/textbook/BeetleGame/src/com/googlecode/android4cs2/beetlegame/BeetleGameActivity.java
@@ -2,6 +2,9 @@ package com.googlecode.android4cs2.beetlegame;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 public class BeetleGameActivity extends Activity {
@@ -9,7 +12,8 @@ public class BeetleGameActivity extends Activity {
 	
 	private TextView turn;
 	private DieView dv;
-	private BeetleSurface surface1;
+	private BeetleView surface1;
+	private BeetleView surface2;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -17,10 +21,13 @@ public class BeetleGameActivity extends Activity {
         setContentView(R.layout.main);
         
         turn = (TextView) findViewById(R.id.turn);
-        surface1 = (BeetleSurface) findViewById(R.id.bug_one);
+        surface1 = (BeetleView) findViewById(R.id.bug_one);
+        surface2 = (BeetleView) findViewById(R.id.bug_two);
         dv = (DieView) findViewById(R.id.die);
         
         surface1.init(dv.getDie());
+        surface2.init(dv.getDie());
         
+        dv.setBugSurfaces(surface1, surface2);
     }
 }
