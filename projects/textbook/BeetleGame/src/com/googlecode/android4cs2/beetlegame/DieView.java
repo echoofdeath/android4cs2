223695452921e3e0e9f1746a50d3ee2160971ef2
jdjennin <jdjennin@7160e6b0-a975-b924-3813-ee829f2ed0ed>
2010-09-21 15:53:08 +0000

    Player switching works, images are greyed out when not that player's turn, and the basis for an end-game notification is in place.

--- projects/textbook/BeetleGame/src/com/googlecode/android4cs2/beetlegame/DieView.java
+++ projects/textbook/BeetleGame/src/com/googlecode/android4cs2/beetlegame/DieView.java
@@ -1,9 +1,12 @@
 package com.googlecode.android4cs2.beetlegame;
 
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.content.Context;
+import android.graphics.Color;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.view.MotionEvent;
 
 public class DieView extends ImageView {
@@ -11,6 +14,9 @@ public class DieView extends ImageView {
 	private Die d;
 	private Beetle[] bugs = new Beetle[2];
 	private BeetleView[] surfaces = new BeetleView[2];
+	private TextView turn;
+	private int[] labels = { R.string.playerone, R.string.playertwo };
+	private int[] winners = { R.string.playerone_win, R.string.playertwo_win };
 	private int player = 0;
 	
 	public DieView(Context context) {
@@ -35,43 +41,65 @@ public class DieView extends ImageView {
 	public boolean onTouchEvent(MotionEvent event) {
 		if ((event.getAction() & MotionEvent.ACTION_MASK) == MotionEvent.ACTION_DOWN) {
 			d.roll();
+			boolean breakOut = false;
 			
 			switch (d.getTopFace()) {
 			case 1:
 				setImageResource(R.drawable.die1);
-				bugs[player].addBody();
+				if (!bugs[player % 2].addBody()) {
+					breakOut = true;
+				}
 				break;
 			case 2:
 				setImageResource(R.drawable.die2);
-				bugs[player].addHead();
+				if (!bugs[player % 2].addHead()) {
+					breakOut = true;
+				}
 				break;
 			case 3:
 				setImageResource(R.drawable.die3);
-				bugs[player].addLeg();
+				if (!bugs[player % 2].addLeg()) {
+					breakOut = true;
+				}
 				break;
 			case 4:
 				setImageResource(R.drawable.die4);
-				bugs[player].addEye();
+				if (!bugs[player % 2].addEye()) {
+					breakOut = true;
+				}
 				break;
 			case 5:
 				setImageResource(R.drawable.die5);
-				bugs[player].addFeeler();
+				if (!bugs[player % 2].addFeeler()) {
+					breakOut = true;
+				}
 				break;
 			case 6:
 				setImageResource(R.drawable.die6);
-				// bugs[player].addTail();
+				// bugs[player % 2].addTail();
 				break;
 			default:
 				break;
 			}
+			
 			invalidate();
 			
-			surfaces[player].updateImages();
-			// player++;
+			if (breakOut) {
+				surfaces[player % 2].setColorFilter(Color.GRAY);
+				surfaces[++player % 2].clearColorFilter();
+				turn.setText(labels[player % 2]);
+				return true;
+			}
 			
-			return true;
+			if (bugs[player % 2].isComplete()) {
+				turn.setText(winners[player % 2]);
+				
+			}
+			
+			surfaces[player % 2].updateImages();
 		}
-		return false;
+		
+		return true;
 	}
 	
 	public Die getDie() {
@@ -86,6 +114,10 @@ public class DieView extends ImageView {
 	public void setBugSurfaces(BeetleView one, BeetleView two) {
 		surfaces[0] = one;
 		surfaces[1] = two;
-		setBugs(one.getBug(), two.getBug()); // must change null back to two.getBug()
+		setBugs(one.getBug(), two.getBug());
+	}
+	
+	public void setTextLabel(TextView tv) {
+		turn = tv;
 	}
 }
