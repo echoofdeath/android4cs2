535aff593f6983eb8807b4a3711425aa1b9dbf28
jdjennin <jdjennin@7160e6b0-a975-b924-3813-ee829f2ed0ed>
2010-09-22 18:46:40 +0000

    Added new game and help options menu. End-game works now. No more user interactions after game is over. Activity for help also included.

--- projects/textbook/BeetleGame/src/com/googlecode/android4cs2/beetlegame/BeetleGameActivity.java
+++ projects/textbook/BeetleGame/src/com/googlecode/android4cs2/beetlegame/BeetleGameActivity.java
@@ -1,8 +1,11 @@
 package com.googlecode.android4cs2.beetlegame;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
@@ -27,5 +30,40 @@ public class BeetleGameActivity extends Activity {
         
         dv.setBugSurfaces(surface1, surface2);
         dv.setTextLabel(turn);
+    }
+    
+    /**
+     * Inflates the Options Menu.
+     * @param menu The menu which is being inflated.
+     * @return boolean
+     */
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	getMenuInflater().inflate(R.menu.menu, menu);
+    	return super.onCreateOptionsMenu(menu);
+    }
+    
+    /**
+     * Called when an item from the Options Menu is selected.
+     * @param item The item which was selected.
+     * @return boolean
+     */
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()) {
+    	// If the user wants a new game, we'll give him one
+    	case R.id.newGame:
+    		newGame();
+    		return true;
+    	// Otherwise, if they need help, they can have that too
+    	case R.id.help:
+    		Intent i = new Intent(BeetleGameActivity.this, HelpActivity.class);
+    		startActivity(i);
+    		return true;
+    	default:
+    		return false;
+    	}
+    }
+    
+    public void newGame() {
+    	dv.newGame();
     }
 }
