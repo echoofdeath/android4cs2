0d0ede93afe994ee83110b5c4a7c046523b78857
jdjennin <jdjennin@7160e6b0-a975-b924-3813-ee829f2ed0ed>
2010-09-23 04:26:23 +0000

    Created basic project with forced landscape orientation and no orientation changes possible.

---
+++ trunk/projects/textbook/IdiotsDelight/src/com/googlecode/android4cs2/idiotsdelight/IdiotsDelightActivity.java
@@ -0,0 +1,13 @@
+package com.googlecode.android4cs2.idiotsdelight;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IdiotsDelightActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
