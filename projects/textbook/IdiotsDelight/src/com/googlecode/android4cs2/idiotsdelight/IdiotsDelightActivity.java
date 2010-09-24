ca8d110919e8720f629c516ff0a6da7db95dc899
jdjennin <jdjennin@7160e6b0-a975-b924-3813-ee829f2ed0ed>
2010-09-24 21:20:37 +0000

    Help and new game framework in place.

--- projects/textbook/IdiotsDelight/src/com/googlecode/android4cs2/idiotsdelight/IdiotsDelightActivity.java
+++ projects/textbook/IdiotsDelight/src/com/googlecode/android4cs2/idiotsdelight/IdiotsDelightActivity.java
@@ -1,7 +1,10 @@
 package com.googlecode.android4cs2.idiotsdelight;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class IdiotsDelightActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,40 @@ public class IdiotsDelightActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    /**
+     * Inflates the Options Menu.
+     * @param menu The menu which is being inflated.
+     * @return boolean
+     */
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	getMenuInflater().inflate(R.menu.menu, menu);
+    	return super.onCreateOptionsMenu(menu);
+    }
+    
+    /**
+     * Called when an item from the Options Menu is selected.
+     * @param item The item which was selected.
+     * @return boolean
+     */
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()) {
+    	// If the user wants a new game, we'll give him one
+    	case R.id.newGame:
+    		newGame();
+    		return true;
+    	// Otherwise, if they need help, they can have that too
+    	case R.id.help:
+    		Intent i = new Intent(IdiotsDelightActivity.this, HelpActivity.class);
+    		startActivity(i);
+    		return true;
+    	default:
+    		return false;
+    	}
+    }
+    
+    public void newGame() {
+    	
     }
 }
