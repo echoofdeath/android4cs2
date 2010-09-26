bfecb95eb80fa358c0832827717ef88b7f3aab08
jdjennin <jdjennin@7160e6b0-a975-b924-3813-ee829f2ed0ed>
2010-09-26 15:32:14 +0000

    Limit of two cards selected functions. Suggestions for a better way to handle this are welcome.

--- trunk/projects/textbook/IdiotsDelight/src/com/googlecode/android4cs2/idiotsdelight/CardView.java
+++ trunk/projects/textbook/IdiotsDelight/src/com/googlecode/android4cs2/idiotsdelight/CardView.java
@@ -1,15 +1,15 @@
 package com.googlecode.android4cs2.idiotsdelight;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
 public class CardView extends ImageView {
 
 	private Stack<Card> s;
-	private int stackNum;
-	
-	private boolean highlight = false;
+	private boolean selected = false;
 	
 	private int[] cardIDs = { R.drawable.x1, R.drawable.x2, R.drawable.x3, R.drawable.x4, R.drawable.x5, R.drawable.x6, R.drawable.x7, R.drawable.x8, R.drawable.x9, R.drawable.x10, R.drawable.x11, R.drawable.x12, R.drawable.x13, R.drawable.x14, R.drawable.x15, R.drawable.x16, R.drawable.x17, R.drawable.x18, R.drawable.x19, R.drawable.x20, R.drawable.x21, R.drawable.x22, R.drawable.x23, R.drawable.x24, R.drawable.x25, R.drawable.x26, R.drawable.x27, R.drawable.x28, R.drawable.x29, R.drawable.x30, R.drawable.x31, R.drawable.x32, R.drawable.x33, R.drawable.x34, R.drawable.x35, R.drawable.x36, R.drawable.x37, R.drawable.x38, R.drawable.x39, R.drawable.x40, R.drawable.x41, R.drawable.x42, R.drawable.x43, R.drawable.x44, R.drawable.x45, R.drawable.x46, R.drawable.x47, R.drawable.x48, R.drawable.x49, R.drawable.x50, R.drawable.x51, R.drawable.x52 };
 	
@@ -25,21 +25,8 @@ public class CardView extends ImageView {
 		super(context, attrs, defStyle);
 	}
 	
-	public void setStack(Stack<Card> s, int stackNum) {
+	public void setStack(Stack<Card> s) {
 		this.s = s;
-		this.stackNum = stackNum;
-	}
-	
-	public void toggleHighlight() {
-		highlight = !highlight;
-	}
-	
-	public boolean isHighlighted() {
-		return highlight;
-	}
-	
-	public int getStackNum() {
-		return stackNum;
 	}
 	
 	public void updateImages() {
@@ -66,6 +53,23 @@ public class CardView extends ImageView {
 			setImageResource(cardIDs[52 - (s.peek().getRank() - 1)*4 + (s.peek().getSuit()+1)]);
 		}
 		
+		if (selected) {
+			setAlpha(150);
+		}
+		
 		invalidate();
+	}
+	
+	public boolean isSelected() {
+		return selected;
+	}
+	
+	public void toggleSelected() {
+		selected = !selected;
+		if (selected) {
+			setAlpha(150);
+			return;
+		}
+		setAlpha(255);
 	}
 }
