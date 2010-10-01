511f30e911e4c7f8e8103d3e0cf3251c26c1d5fd
jdjennin <jdjennin@7160e6b0-a975-b924-3813-ee829f2ed0ed>
2010-10-01 03:00:25 +0000

    Basic GUI framework laid out. Functional options menu.

---
+++ trunk/projects/textbook/War/src/com/googlecode/android4cs2/war/DeckView.java
@@ -0,0 +1,37 @@
+package com.googlecode.android4cs2.war;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class DeckView extends ImageView {
+
+	private Queue<Card> q;
+	
+	public DeckView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DeckView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DeckView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void updateImages() {
+		if (!q.isEmpty()) {
+			setImageResource(R.drawable.xb1fv);
+		} else {
+			setImageResource(R.drawable.blank);
+		}
+	}
+	
+	public void setQ(Queue<Card> q) {
+		this.q = q;
+	}
+}
---
+++ trunk/projects/textbook/War/src/com/googlecode/android4cs2/war/HelpActivity.java
@@ -0,0 +1,15 @@
+package com.googlecode.android4cs2.war;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/** Offers the user some help on playing */
+public class HelpActivity extends Activity {
+	
+	/** Called when the activity is first created */
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.help_main);
+	}
+	
+}
