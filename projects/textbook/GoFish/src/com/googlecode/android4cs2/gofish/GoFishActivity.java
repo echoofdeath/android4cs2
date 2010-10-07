f23760e7ae2c5f6c725b2351d65ba17f6b8daf4a
jdjennin <jdjennin@7160e6b0-a975-b924-3813-ee829f2ed0ed>
2010-10-07 16:25:00 +0000

    Basic project configuration with Options Menu and HelpActivity.

---
+++ projects/textbook/GoFish/src/com/googlecode/android4cs2/gofish/GoFishActivity.java
@@ -0,0 +1,49 @@
+package com.googlecode.android4cs2.gofish;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class GoFishActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    
+    /**
+     * Inflates the Options Menu.
+     * @param menu The menu which is being inflated.
+     * @return boolean
+     */
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	getMenuInflater().inflate(R.menu.menu, menu);
+    	return super.onCreateOptionsMenu(menu);
+    }
+    
+    /**
+     * Called when an item from the Options Menu is selected.
+     * @param item The item which was selected.
+     * @return boolean
+     */
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()) {
+    	// If the user wants a new game, we'll give him one
+    	case R.id.newGame:
+    		newGame();
+    		return true;
+    	// Otherwise, if they need help, they can have that too
+    	case R.id.help:
+    		Intent i = new Intent(GoFishActivity.this, HelpActivity.class);
+    		startActivity(i);
+    		return true;
+    	default:
+    		return false;
+    	}
+    }
+
+    public void newGame() {}
+}
