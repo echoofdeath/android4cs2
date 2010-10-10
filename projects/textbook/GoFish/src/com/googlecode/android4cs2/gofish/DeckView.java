dc1168c22ece2df8f129f2476c21d658a30f9692
jdjennin <jdjennin@7160e6b0-a975-b924-3813-ee829f2ed0ed>
2010-10-10 04:01:33 +0000

    Created testing layout, started declaring the Views and Models in GoFishActivity. Changed DeckView to use a Deck object instead of a Queue of Cards.

--- projects/textbook/GoFish/src/com/googlecode/android4cs2/gofish/DeckView.java
+++ projects/textbook/GoFish/src/com/googlecode/android4cs2/gofish/DeckView.java
@@ -1,14 +1,12 @@
 package com.googlecode.android4cs2.gofish;
 
-import java.util.Queue;
-
 import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
 public class DeckView extends ImageView {
 
-	private Queue<Card> q;
+	private Deck d;
 	
 	public DeckView(Context context) {
 		super(context);
@@ -26,14 +24,14 @@ public class DeckView extends ImageView {
 	}
 	
 	public void updateImages() {
-		if (!q.isEmpty()) {
+		if (!d.isEmpty()) {
 			setImageResource(R.drawable.xb1fv);
 		} else {
 			setImageResource(R.drawable.blank);
 		}
 	}
 	
-	public void setQ(Queue<Card> q) {
-		this.q = q;
+	public void setDeck(Deck d) {
+		this.d = d;
 	}
 }
