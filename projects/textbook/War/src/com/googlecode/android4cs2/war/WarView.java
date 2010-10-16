3386854efec3db781ba8c6c44045b622464ab915
jdjennin <jdjennin@7160e6b0-a975-b924-3813-ee829f2ed0ed>
2010-10-16 18:19:57 +0000

    Rewrote WarView and WarActivity to draw and use 4 cards in a War, also trying to sleep before redrawing the WarViews. Broken. Also no code yet for war-triggered wars.

--- projects/textbook/War/src/com/googlecode/android4cs2/war/WarView.java
+++ projects/textbook/War/src/com/googlecode/android4cs2/war/WarView.java
@@ -1,16 +1,19 @@
 package com.googlecode.android4cs2.war;
 
+import java.util.Stack;
+
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.widget.ImageView;
 
 public class WarView extends ImageView {
 
-	private Queue<Card> q;
+	private Card card;
 	
 	public WarView(Context context) {
 		super(context);
@@ -27,67 +30,66 @@ public class WarView extends ImageView {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public void setQ(Queue<Card> q) {
-		this.q = q;
+	public void setCard(Card c) {
+		this.card = c;
 	}
 	
 	public void clear() {
 		setImageResource(R.drawable.background);
 	}
 	
-	public void updateImages() {
-		if (q == null || q.isEmpty()) {
+	public void updateImages(int numCards) {
+		if (card == null) {
 			setImageResource(R.drawable.background);
 		} else {
-			Bitmap output = Bitmap.createBitmap(102, 96, Bitmap.Config.ARGB_8888);
+			Bitmap b = BitmapFactory.decodeResource(getResources(), R.drawable.xb1pl);
+			Bitmap output = Bitmap.createBitmap(b.getWidth()*(numCards-1)+72, 96, Bitmap.Config.ARGB_8888);
 			Canvas c = new Canvas(output);
 			
-			reverse();
-			
 			int offset = 0;
-			for (int i = 0; i < 3; i++) {
-				Card k = q.remove();
-				Bitmap b = BitmapFactory.decodeResource(getResources(), R.drawable.background);
-
-				if (k.getRank() == 1) {
-					switch (k.getSuit()) {
-					case Card.SPADES:
-						b = BitmapFactory.decodeResource(getResources(), R.drawable.x2);
-						break;
-					case Card.HEARTS:
-						b = BitmapFactory.decodeResource(getResources(), R.drawable.x3);
-						break;
-					case Card.DIAMONDS:
-						b = BitmapFactory.decodeResource(getResources(), R.drawable.x4);
-						break;
-					case Card.CLUBS:
-						b = BitmapFactory.decodeResource(getResources(), R.drawable.x1);
-						break;
-					default:
-						setImageResource(R.drawable.background);
-						break;
-					}
-				} else {
-					b = BitmapFactory.decodeResource(getResources(), CardView.cardIDs[52 - (k.getRank() - 1)*4 + (k.getSuit()+1)%4]);
-				}
-				
+			for (int i = 0; i < numCards-1; i++) {
 				c.drawBitmap(b, offset, 0, null);
-				offset += 10;
-				q.add(k);
+				offset += b.getWidth();
 			}
 			
-			reverse();
+			b = BitmapFactory.decodeResource(getResources(), R.drawable.background);
+
+			if (card.getRank() == 1) {
+				switch (card.getSuit()) {
+				case Card.SPADES:
+					b = BitmapFactory.decodeResource(getResources(), R.drawable.x2);
+					break;
+				case Card.HEARTS:
+					b = BitmapFactory.decodeResource(getResources(), R.drawable.x3);
+					break;
+				case Card.DIAMONDS:
+					b = BitmapFactory.decodeResource(getResources(), R.drawable.x4);
+					break;
+				case Card.CLUBS:
+					b = BitmapFactory.decodeResource(getResources(), R.drawable.x1);
+					break;
+				default:
+					setImageResource(R.drawable.background);
+					break;
+				}
+			} else {
+				b = BitmapFactory.decodeResource(getResources(), CardView.cardIDs[52 - (card.getRank() - 1)*4 + (card.getSuit()+1)%4]);
+			}
+			
+			c.drawBitmap(b, offset, 0, null);
 			setImageBitmap(output);
 		}
 	}
 	
-	private void reverse() {
-		Card c1 = q.remove();
-		Card c2 = q.remove();
-		Card c3 = q.remove();
-		q.add(c3);
-		q.add(c2);
-		q.add(c1);
-	}
+/*	private void reverse() {
+		Stack<Card> newQ = new Stack<Card>();
+		while (!q.isEmpty()) {
+			newQ.push(q.remove());
+		}
+		
+		while (!newQ.isEmpty()) {
+			q.add(newQ.pop());
+		}
+	} */
 
 }
