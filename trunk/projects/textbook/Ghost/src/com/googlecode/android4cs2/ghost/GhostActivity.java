7827b5c18fa140025e1a8dd65909547e70db120c
jdjennin <jdjennin@7160e6b0-a975-b924-3813-ee829f2ed0ed>
2010-12-19 02:14:31 +0000

    The only thing remaining is a little more animation (optional) and some bug with the end-game logic and when you enter a word that isn't in the *.txt file.

--- trunk/projects/textbook/Ghost/src/com/googlecode/android4cs2/ghost/GhostActivity.java
+++ trunk/projects/textbook/Ghost/src/com/googlecode/android4cs2/ghost/GhostActivity.java
@@ -8,6 +8,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -32,9 +35,39 @@ public class GhostActivity extends Activity {
 	/** Our pointer to the current node in the word tree */
 	private DigitalNode<Boolean> node;
 	
-	/** Int to keep track of whose turn it is */
-	private int player = 0;
+	private static int USER = 0;
 	
+	private static int COMPUTER = 1;
+	
+	/** Int to keep track of whose turn it is */
+	private int player = USER;
+	
+	/** A set of Animations for the ghost */
+	private AnimationSet[] set = new AnimationSet[2];
+	
+	/** Animation Listener for when the Ghost stops moving */
+	private AnimationListener animListener = new AnimationListener() {
+
+		@Override
+		public void onAnimationEnd(Animation animation) {
+			computerTurn();
+			ghost.startAnimation(set[1]);
+			if (player == 0) {
+				ghost.setVisibility(View.INVISIBLE);
+			}
+		}
+
+		@Override
+		public void onAnimationRepeat(Animation animation) {
+						
+		}
+
+		@Override
+		public void onAnimationStart(Animation animation) {
+			
+		}
+		
+	};
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -46,13 +79,21 @@ public class GhostActivity extends Activity {
         instructions = (TextView) findViewById(R.id.instructions);
         ghost = (ImageView) findViewById(R.id.ghost);
         
+        for (int i = 0; i < 2; i++) {
+        	set[i] = new AnimationSet(true);
+        }
+        
+        set[0].setAnimationListener(animListener);
+        set[0].addAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_in));
+        set[1].addAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_out));      
+        
         newGame();
     }
     
     @Override
     public boolean onKeyUp(int keyCode, KeyEvent event) {
 		Log.d("onKeyUp", "Key code: " + (char)event.getUnicodeChar());
-		if (player == 0) {
+		if (player == USER) {
 			if (event.getUnicodeChar() >= 97 && event.getUnicodeChar() <= 122) {
 				word += (char)event.getUnicodeChar();
 			} else {
@@ -64,10 +105,14 @@ public class GhostActivity extends Activity {
 				root = loadNodes();
 				node = root;
 			}
-			player = 1 - player;
+			player = COMPUTER;
+			node = node.getChild((char)event.getUnicodeChar());
 			wordView.setText(word);
-			computerTurn();
-//			wordView.startAnimation(fadeOut); When this is done, setText of wordView to new word and fade back in
+			
+			// **********Test
+			ghost.setVisibility(View.VISIBLE);
+			ghost.startAnimation(set[0]);
+			
 		}
 		return false;
 	}
@@ -83,8 +128,18 @@ public class GhostActivity extends Activity {
     }
     
     public void computerTurn() {
+    	if (node.isLeaf()) {
+    		ghost.setImageResource(R.drawable.ghostdead);
+    		ghost.startAnimation(set[0]);
+    		instructions.setText("Player wins!");
+    		return;
+    	}
+    	char letter = node.randomLetter();
+    	word += letter;
+    	wordView.setText(word);
+    	node = node.getChild(letter);
     	
-    	player = 1 - player;
+    	player = USER;
     }
     
     public DigitalNode<Boolean> loadNodes() {
@@ -113,8 +168,8 @@ public class GhostActivity extends Activity {
     			child = new DigitalNode<Boolean>(false);
     			addTo.setChild(c, child);
     		}
-    		node = child;
+    		addTo = child;
     	}
-    	node.setItem(true);
+    	addTo.setItem(true);
     }
 }
