7ad3d0bf6e2cc9e976583455424103655bfcb0ff
mark.goadrich <mark.goadrich@7160e6b0-a975-b924-3813-ee829f2ed0ed>
2011-01-05 21:15:24 +0000

    Initial Project

---
+++ trunk/projects/external/centenary/WallDrug/src/com/googlecode/android4cs2/walldrug/WallDrug.java
@@ -0,0 +1,13 @@
+package com.googlecode.android4cs2.walldrug;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WallDrug extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
